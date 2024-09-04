f0bb1e2dd168201ccc950d1daf56eac3ce0d626a
Bwar3am <barsamhassanzadehaski@gmail.com>
2024-09-04 11:22:13 +0330

    ss
    ss

--- arrays/insertion sort/insertion.cpp
+++ arrays/insertion sort/insertion.cpp
@@ -4,19 +4,17 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-void insertionSort(int arr[], int n){
+void insertionSort(int arr[], int n){           
     for (int i = 1; i < n; i++) {
         int insert_index = arr[i];
         int j = i - 1;
 
-        /* Move elements of arr[0..i-1], that are
-           greater than key, to one position ahead
-           of their current position */
+        
         while (j >= 0 && arr[j] > insert_index) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j = j - 1; 
         }
-        arr[j + 1] = insert_index;
+        arr[j + 1] = insert_index; 
     }
 }
 
